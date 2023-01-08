72681f468bc8b062fac65c25c42c67400c59360f
Felipe Lunkes <felipenldev@gmail.com>
2023-01-08 01:33:51 -0300

    v0.3

--- RAU.py
+++ RAU.py
@@ -17,85 +17,101 @@ class RAU:
         self.lbl3=Label(win, text='Configuration options')
         self.lbl4=Label(win, text='Running options')
 
-        self.botao0=Button(win, text='Run Version 1 UNIX for PDP-11', command=self.runV1UNIX)
-        self.botao1=Button(win, text='Run Version 5 UNIX for PDP-11', command=self.runV5UNIX)
-        self.botao2=Button(win, text='Run Version 7 UNIX for PDP-11', command=self.runV7UNIX)
-        self.botao3=Button(win, text='Run 2.11BSD UNIX for PDP-11', command=self.run211BSD)
-        self.botao4=Button(win, text='Run Version 7 UNIX for x86', command=self.runV7UNIXx86)
-        self.botao5=Button(win, text='Install UNIX system images', command=self.installImages)
-        self.botao6=Button(win, text="Clear temporary files", command=self.clearTemp)
+        self.btn0=Button(win, text='Run Version 1 UNIX for PDP-11', command=self.runV1UNIX)
+        self.btn1=Button(win, text='Run Version 5 UNIX for PDP-11', command=self.runV5UNIX)
+        self.btn2=Button(win, text='Run Version 7 UNIX for PDP-11', command=self.runV7UNIX)
+        self.btn3=Button(win, text='Run 2.11BSD UNIX for PDP-11', command=self.run211BSD)
+        self.btn4=Button(win, text='Run Version 7 UNIX for x86', command=self.runV7UNIXx86)
+        self.btn5=Button(win, text='Install UNIX system images', command=self.installImages)
+        self.btn6=Button(win, text="Clear temporary files", command=self.clearTemp)
 
         self.lbl1.place(x=170, y=5)
         self.lbl2.place(x=100, y=50)
         self.lbl3.place(x=180, y=100)
 
-        self.botao5.place(x=10, y=140)
-        self.botao6.place(x=250, y=140)
+        self.btn5.place(x=10, y=140)
+        self.btn6.place(x=250, y=140)
 
         self.lbl4.place(x=200, y=200)
 
-        self.botao0.place(x=10, y=240)
-        self.botao1.place(x=250, y=240)
-        self.botao2.place(x=10, y=280)
-        self.botao3.place(x=250, y=280)
-        self.botao4.place(x=10, y=320)
+        self.btn0.place(x=10, y=240)
+        self.btn1.place(x=250, y=240)
+        self.btn2.place(x=10, y=280)
+        self.btn3.place(x=250, y=280)
+        self.btn4.place(x=10, y=320)
         
 
     def runV1UNIX(self):
     
+        print(" > Version 1 UNIX (PDP-11) was selected to run.")
+
         os.system("gnome-terminal -- bash -c './run.sh v1UNIX'")
 
-        self.terminar() 
+        self.complete() 
     
     def runV5UNIX(self):
 
+        print(" > Version 5 UNIX (PDP-11) was selected to run.")
+
         os.system("gnome-terminal -- bash -c './run.sh v5UNIX'")
 
-        self.terminar() 
+        self.complete() 
 
     def runV7UNIX(self):
 
+        print(" > Version 7 UNIX (PDP-11) was selected to run.")
+
         os.system("gnome-terminal -- bash -c './run.sh v7UNIX'")
 
-        self.terminar() 
+        self.complete() 
 
     def run211BSD(self):
     
+        print(" > 2.11BSD UNIX (PDP-11) was selected to run.")
+
         os.system("gnome-terminal -- bash -c './run.sh 211BSDUNIX'")
 
-        self.terminar() 
+        self.complete() 
 
     def runV7UNIXx86(self):
     
+        print(" > Version 7 UNIX (x86) was selected to run.")
+
         os.system("gnome-terminal -- bash -c './run.sh v7UNIXx86'")
 
-        self.terminar() 
+        self.complete() 
 
     def installImages(self):
 
+        print(" > Installing UNIX system images...")
+
         os.system("gnome-terminal -- bash -c './run.sh installUNIX'")
 
-        self.terminar() 
+        self.complete() 
     
     def clearTemp(self):
 
+        print(" > Clear temporary files.")
+
         os.system("gnome-terminal -- bash -c './run.sh clearTemp'")
 
-        self.terminar() 
+        self.complete() 
 
-    def terminar(self):
+    def complete(self):
 
         print("Request completed.\n")
 
-# 
+# Main code
 
-janela=Tk()
-autobuild=RAU(janela)
-janela.title('Run ancient UNIX v0.2')
-janela.geometry("500x400+10+10")
-print("\nRun ancient UNIX frontend v0.2")
+version='v0.3'
+window=Tk()
+RunAncientUNIX=RAU(window)
+window.title('Run ancient UNIX '+version)
+window.geometry("500x400+10+10")
+print("\nRun ancient UNIX frontend "+version)
 print("Copyright (C) 2023 Felipe Miguel Nery Lunkes")
 print("All rights reserved.")
+print("Licenced under BSD-3-Clause license")
 print("\nWaiting for user interaction...")
-janela.mainloop()
+window.mainloop()
 print("Finished by user.")
